c877d1a3a22a25442fcbd66fb704577bf8b25eed
maklh899 <maklh899@hotmail.com>
2021-02-14 01:49:33 -0800

    adding index and app.jsx

---
+++ App.jsx
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import { reducers } from './reducers';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+export default function App() {
+    return (
+        <Provider store={store}>
+            <View style={styles.container}>
+                <Text>CupofSugar!</Text>
+                <Text>heeeey!</Text>
+                <StatusBar style="auto"/>
+            </View>
+        </Provider>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
