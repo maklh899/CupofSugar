05309e975e383a4b1509d3a98393f60a893751aa
maklh899 <maklh899@hotmail.com>
2021-02-13 20:22:06 -0800

    Setting up enviornment

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # CupofSugar
+---
+*Mobile application designed to compact apartment living essentials into one place and give users the ability to communicate with neighbors within their complex. Management will have a user friendly interface to easily oversee their residents.*
+
+## Getting Started
+### Frameworks and tools:
+- React-Native
+- Node.js
+- Express
+- Mongoose and MongoDB
+- ESLint - AirBnB Styleguide
+- GitHub Actions
+
+### Software needed:
+- Node / npm
+- Expo
+- MongoDB Atlas
+
+### To run a local instance of the database and API:
+- Change directory to HappyHousePlants repository
+
+`cd HappyHousePlants`
+
+- Run the commands
+
+```
+npm install
+cd ../frontend && npm install
+expo start
+```
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+//configures our .env file
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8070;
+
+
+app.use(cors()); //cors middleware
+app.use(express.json()); //allows us to parse json
+
+const uri = process.env.ATLAS_URI;
+//connects to the uri that our database is stored at
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+// const exercisesRouter = require('./routes/exercises');
+// const usersRouter = require('./routes/users');
+
+// app.use('/exercises', exercisesRouter);
+// app.use('/users', usersRouter);
+
+//starts the server, and listens on a certain port
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
