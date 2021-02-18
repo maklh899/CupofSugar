956014f45891e29feebb799caacfc7c65156b299
maklh899 <maklh899@hotmail.com>
2021-02-17 23:19:55 -0800

    edited user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,30 +2,30 @@ const mongoose = require('mongoose');
 
 mongoose.pluralize(null);
 const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3
-  },
-  userId: { type: String, index: true },
-  password: String, // Salted+Hashed
-  admin: Boolean,
-  
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+    },
+    userId: { type: String, index: true },
+    password: String, // Salted+Hashed
+    admin: Boolean,
+
 },
 {
-    timestamps: true
+    timestamps: true,
 });
 
 const USERS = mongoose.model('Users', userSchema);
